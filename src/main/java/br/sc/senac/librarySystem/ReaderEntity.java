ac9243436e3601c09f1240b7ea90cd9423c2d1c9
LucasTody <estudante34756@gmail.com>
2020-04-24 21:21:43 -0300

    Versão beta: Implementada a classe ReaderEntity e modelada

---
+++ src/main/java/br/sc/senac/librarySystem/ReaderEntity.java
@@ -0,0 +1,59 @@
+package br.sc.senac.librarySystem;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "readerEntity")
+public class ReaderEntity implements Serializable {
+
+	private static final long serialVersionUID = -1384495822813041468L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long readerId;
+	
+	private String entityReaderName;
+	
+	private Integer entityReaderAge;
+	
+	public ReaderEntity(String readerName, Integer readerAge) {
+		this.entityReaderName = readerName;
+		this.entityReaderAge = readerAge;
+	}
+	
+	protected ReaderEntity() {
+		
+	}
+	
+	public String toString() {
+		return "Nome: " + this.entityReaderName + "Idade: " + this.entityReaderAge;
+	}
+
+	public String getEntityReaderName() {
+		return entityReaderName;
+	}
+
+	public void setEntityReaderName(String entityReaderName) {
+		if(entityReaderName != null) {
+		this.entityReaderName = entityReaderName;
+		}
+	}
+
+	public Integer getEntityReaderAge() {
+		return entityReaderAge;
+	}
+
+	public void setEntityReaderAge(Integer entityReaderAge) {
+		if(entityReaderAge != null) {
+		this.entityReaderAge = entityReaderAge;
+		}
+	}
+	
+	public Long getReaderId() {
+		return this.readerId;
+	}
+}
