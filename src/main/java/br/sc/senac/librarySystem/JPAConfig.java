20d04cb1b69d18efc6854c56c5192d6076cca0c1
LucasTody3535 <lucas.web3535@gmail.com>
2020-04-29 11:18:27 -0300

    Refatorada a classe JPAConfig

--- src/main/java/br/sc/senac/librarySystem/JPAConfig.java
+++ src/main/java/br/sc/senac/librarySystem/JPAConfig.java
@@ -5,14 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class JPAConfig {
-
-	@Configuration
-	@EnableJpaAuditing(auditorAwareRef = "auditorAware")
-	public class JpaConfig {
-		@Bean
-		public AuditorAware<String> auditorAware() {
-			return new AuditorAwareImpl();
-		}
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new AuditorAwareImpl();
 	}
 }
