e995c0d6975019a86e8e3f9a3304d9de2aa04c40
LucasTody3535 <lucas.web3535@gmail.com>
2020-04-30 09:27:49 -0300

    Adicionado os getters e setters a classe Auditable

--- src/main/java/br/sc/senac/librarySystem/Auditable.java
+++ src/main/java/br/sc/senac/librarySystem/Auditable.java
@@ -16,15 +16,46 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable<U> {
-@CreatedBy
-    protected U createdBy;
-@CreatedDate
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date creationDate;
-@LastModifiedBy
-    protected U lastModifiedBy;
-@LastModifiedDate
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date lastModifiedDate;
-}
+	@CreatedBy
+	protected U createdBy;
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date creationDate;
+	@LastModifiedBy
+	protected U lastModifiedBy;
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date lastModifiedDate;
 
+	public U getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(U createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public U getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(U lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+}
