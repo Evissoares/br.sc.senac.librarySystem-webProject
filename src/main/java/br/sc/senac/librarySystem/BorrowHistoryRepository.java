56a4cfe2e4561db6811bb554dd309bd8a06875c2
LucasTody3535 <lucas.web3535@gmail.com>
2020-05-04 09:23:23 -0300

    Deletado classes desnecessárias

--- src/main/java/br/sc/senac/librarySystem/BorrowHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package br.sc.senac.librarySystem;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BorrowHistoryRepository extends CrudRepository<BorrowHistoryEntity, Long> {
-
-}
